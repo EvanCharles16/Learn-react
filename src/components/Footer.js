9143f4e2a9053f586c681446c1a05b8ac91b33e4
EvanCharles16 <evancharles1609@gmail.com>
2020-04-13 19:09:25 +0700

    FIXED

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,17 @@ class Footer extends Component {
   render() {
     return (
       <div className="contact" id="contact">
-        
+
+        <div className="contactTitle">
+            <h1>Contact Me</h1>
+        </div>
+
+        <div className="social-media-icon">
+          <a href="#"><i className="fa fa-facebook fa-4x"></i></a>
+          <a href="#"><i className="fa fa-whatsapp fa-4x"></i></a>
+          <a href="#"><i className="fa fa-twitter fa-4x"></i></a>
+          <a href="#"><i className="fa fa-youtube fa-4x"></i></a>
+        </div>
 
         <div class="footer">
             <a href="#">&copy; Designed By Me 2020</a>
