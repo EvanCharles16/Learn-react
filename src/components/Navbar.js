8e1e3f0f414283023478c8aeabc57ad4d6a75b5b
EvanCharles16 <evancharles1609@gmail.com>
2020-04-13 14:11:45 +0700

    learn-react

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render(){
+        return (
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Buy me a coffee</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
