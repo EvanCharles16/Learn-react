b56fab00d45a2117321586f52d24df663cd64e9d
EvanCharles16 <evancharles1609@gmail.com>
2020-04-17 01:09:59 +0700

    FIXED

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
-import './Footer.css'
+import "./Footer.css";
 
 class Footer extends Component {
   render() {
     return (
       <div className="contact" id="contact">
-
         <div className="contactTitle">
-            <h1>Contact Me</h1>
+          <h1>Contact Me</h1>
         </div>
 
-        <div className="social-media-icon">
-          <a href="#"><i className="fa fa-facebook fa-4x"></i></a>
-          <a href="#"><i className="fa fa-whatsapp fa-4x"></i></a>
-          <a href="#"><i className="fa fa-twitter fa-4x"></i></a>
-          <a href="#"><i className="fa fa-youtube fa-4x"></i></a>
-        </div>
+        <footer className="social-media-icon">
+          <a href="#">
+            <i className="fa fa-facebook fa-4x"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-whatsapp fa-4x"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter fa-4x"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-youtube fa-4x"></i>
+          </a>
+        </footer>
 
         <div class="footer">
-            <a href="#">&copy; Designed By Me 2020</a>
+          <a href="#">&copy; Designed By Me 2020</a>
         </div>
       </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Content from '../components/Content'
+import React, { Component } from "react";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/Footer";
+import Content from "../components/Content";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar/>
-                <Content/>
-                <Footer/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Home;
